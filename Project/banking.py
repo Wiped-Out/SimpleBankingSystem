73586b43d0f6322319936776ed52a5e8583f8125
Wiped Out <mauromathers@icloud.com>
2020-10-28 03:57:28 -0300

    Now the bank access the acc via a Context Manager

--- Project/banking.py
+++ Project/banking.py
@@ -1,29 +1,12 @@
 #!/usr/bin/env python3
 from random import randint
 import sqlite3
-from typing import Callable, Optional, Tuple
+from typing import Optional, Tuple
 
 
 class BankingSystem:
     def __init__(self) -> None:
         self.database()
-
-    class Login:
-        """Asks for credentials to the user before letting him access to sensitive info."""
-
-        def __init__(self, f: Callable[['BankingSystem', str], None]) -> None:
-            self.f = f
-
-        def __call__(self):
-            card = input('Enter your card number:\n')
-            PIN: str = input('Enter your PIN:\n')
-            if BankingSystem.check_credentials(card, PIN):
-                print('You have successfully logged in!\n')
-                self.f(BankingSystem, card, PIN)
-                print('You have successfully logged out.\n')
-                return
-            else:
-                print('Wrong card number or PIN\n')
 
     def menu(self) -> None:
         """Main driver of the program."""
@@ -33,7 +16,13 @@ class BankingSystem:
             if choice == '1':
                 self.create_account()
             elif choice == '2':
-                self.account()
+                card = input('Enter your card number:\n')
+                PIN: str = input('Enter your PIN:\n')
+                try:
+                    with Account(card, PIN) as acc:
+                        acc.menu()
+                except ValueError as e:  # Will catch the exception raised when the card number or PIN is wrong and will print the msg.
+                    print(e)
             elif choice == '0':
                 print('Bye!')
                 exit()
@@ -44,7 +33,6 @@ class BankingSystem:
     def database(
         card: Optional[str] = None,
         pin: Optional[str] = None,
-        balance: Optional[int] = None,
     ) -> None:
         """Creates a db if no parameters are provided, else creates account."""
         with sqlite3.connect('card.s3db') as data:
@@ -63,25 +51,24 @@ class BankingSystem:
                 cursor = data.cursor()
                 cursor.execute(
                     '''
-                INSERT OR IGNORE INTO card (number, pin, balance)
+                INSERT OR IGNORE INTO card (number, pin)
                 VALUES (?, ?, ?);
                 ''',
-                    (card, pin, balance),
+                    (card, pin),
                 )
 
     @staticmethod
     def check_credentials(card: str, pin: str) -> bool:
-        """Gets the credentials of a card on a db"""
+        """Gets the credentials of a card from the db"""
         with sqlite3.connect('card.s3db') as data:
             cursor = data.cursor()
             cursor.execute(
                 '''
-            SELECT pin FROM card WHERE number = (?) and pin = (?);
+            SELECT pin FROM card WHERE number = (?) AND pin = (?);
             ''',
                 (card, pin),
             )
-            result = cursor.fetchone()
-            return bool(result)
+            return bool(cursor.fetchone())
 
     @staticmethod
     def luhn_algorithm(card_number: str) -> bool:
@@ -95,19 +82,18 @@ class BankingSystem:
 
     @staticmethod
     def generate_numbers() -> Tuple[str, str]:
-        """Generates numbers until it passes the Luhn algorithm"""
+        """Generates numbers until it passes Luhn's algorithm"""
         while True:
             random_card = ''.join(['400000'] + [str(randint(0, 9)) for _ in range(11)])
             random_PIN = ''.join([str(randint(0, 9)) for _ in range(4)])
-            if not BankingSystem.check_credentials(random_card, random_PIN):
-                if BankingSystem.luhn_algorithm(random_card):
-                    return random_card, random_PIN
+            if BankingSystem.luhn_algorithm(random_card):
+                return random_card, random_PIN
             else:
                 continue
 
     def create_account(self) -> None:
         card, PIN = self.generate_numbers()
-        self.database(card, PIN, 0)
+        self.database(card, PIN)
         print('\nYour card has been created')
         print(f'Your card number:\n{card}')
         print(f'Your card PIN:\n{PIN}\n')
@@ -117,7 +103,6 @@ class BankingSystem:
         From: Optional[str] = None,
         to: Optional[str] = None,
         amount: Optional[int] = None,
-        close: Optional[bool] = False,
     ) -> str:
         """Gets, updates and deletes entries from the database"""
         with sqlite3.connect('card.s3db') as data:
@@ -136,68 +121,94 @@ class BankingSystem:
                     (amount, From),
                 )
                 return 'Success!'
-            elif From and amount:
-                cur.execute(
-                    '''
-                UPDATE card SET balance = (balance + ?) WHERE number LIKE (?);
-                ''',
-                    (amount, From),
-                )
-                return 'Income was added!'
-            elif close:
-                cur.execute(
-                    '''
-                DELETE FROM card where number = (?);
-                ''',
-                    (From,),
-                )
-            else:
-                cur.execute(
-                    '''SELECT balance FROM card WHERE number LIKE (?);''', (From,)
-                )
-                return cur.fetchone()[0]
         return 'Error.'
 
-    @Login
-    def account(self, card: Optional[str] = None, pin: Optional[str] = None) -> None:
+
+class Account:
+    __slots__ = ('card', 'pin', 'db')
+
+    def __init__(self, card: str, pin: str) -> None:
+        self.card, self.pin = card, pin
+        self.db = sqlite3.connect('card.s3db').cursor()
+
+    def exists(self, card: str) -> bool:
+        self.db.execute('SELECT number FROM card WHERE number = (?);', (card,))
+        return bool(self.db.fetchone())
+
+    def balance(self) -> int:
+        self.db.execute('SELECT balance FROM card WHERE number = (?);', (self.card,))
+        return self.db.fetchone()[0]
+
+    def add_balance(self, balance: int) -> str:
+        self.db.execute(
+            'UPDATE card SET balance = (balance + ?) WHERE number = (?);',
+            (balance, self.card),
+        )
+        return 'Income was added!'
+
+    def close_account(self) -> None:
+        self.db.execute('DELETE FROM card WHERE number = (?);', (self.card,))
+
+    def menu(self) -> None:
+        options = {
+            '1': 'Balance',
+            '2': 'Add income',
+            '3': 'Do transfer',
+            '4': 'Close Account',
+            '5': 'Log out',
+            '0': 'Exit',
+        }
         while True:
-            print(
-                '1. Balance\n2. Add income\n3. Do transfer\n4. Close account\n5. Log out\n0. Exit'
-            )
+            for k, v in options.items():
+                print(f'{k}. {v}')
             choice: str = input()
-            if choice == '1':
-                print(f"\nBalance: {self.get_update(card)}\n")
+            if choice not in options:
+                print('Unknown option.')
+                continue
+            elif choice == '1':
+                print(f"\nBalance: {self.balance()}\n")
             elif choice == '2':
                 income: int = int(input('Enter income:\n'))
-                print(self.get_update(From=card, amount=income))
+                print(self.add_balance(income))
             elif choice == '3':
                 to: str = input('Enter card number:\n')
-                if card == to:
+                if self.card == to:
                     print('You can\'t transfer money to the same account!\n')
-                elif not self.luhn_algorithm(to):
+                elif not BankingSystem.luhn_algorithm(to):
                     print(
                         'You probably made a mistake in the card number. Please try again!\n'
                     )
-                elif not self.check_credentials(to):
+                elif not self.exists(to):
                     print('Such card does not exist.\n')
                 else:
                     amount: int = int(
                         input('Enter how much money you want to transfer:\n')
                     )
-                    if amount > int(self.get_update(card)):
+                    if amount > int(self.balance()):
                         print('Not enough money!\n')
                         continue
-                    print(self.get_update(card, to, amount))
+                    print(BankingSystem.get_update(self.card, to, amount))
             elif choice == '4':
-                self.get_update(From=card, close=True)
+                self.close_account()
                 return
             elif choice == '5':
-                return
+                raise Exception('You have successfully logged out.')
             elif choice == '0':
-                print('Bye!')
-                exit()
-            else:
-                print('Unknown option.\n')
+                raise SystemExit('Bye!')
+
+    def __enter__(self):
+        if not BankingSystem.check_credentials(self.card, self.pin):
+            raise ValueError('Wrong card number or PIN.\n')
+        print('You have successfully logged in!\n')
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.db.close()
+        if type == SystemExit:
+            raise SystemExit(value)
+        if type:
+            print(value)
+            return True
 
 
 if __name__ == '__main__':
